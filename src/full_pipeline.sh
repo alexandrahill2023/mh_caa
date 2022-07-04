85f931482b00f186cf3d958b3d02512c637f5f21
Alexandra Hill <alexandralaurahill@gmail.com>
2022-07-04 19:55:20 -0400

    Update full_pipeline.sh

--- src/full_pipeline.sh
+++ src/full_pipeline.sh
@@ -2,7 +2,7 @@
 # Running this script end-to-end has NOT been tested
 
 # These directories need to be pre-created as specified
-RAW_ARTICLES_DIR="..data/black_fem/" # Each article should be in a separate file, where the filename is article_id.txt
+RAW_ARTICLES_DIR="..data/black_fem" # Each article should be in a separate file, where the filename is article_id.txt
 STANFORD_DIR="..data/stanford-parser-full-2020-11-17" # Downloaded Stanford parser
 
 
